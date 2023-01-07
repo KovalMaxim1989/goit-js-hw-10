259257f32fb3486e7b204fbd3ed5c6bc8166cfc2
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2023-01-07 17:03:01 +0200

    start
    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { fetchCountries } from './js/fetchCountries';
 
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+
+fetch('https://restcountries.com/v2/name/uk?fields=name,capital,flag,flags,population,languages')
+    .then(response => {
+        return response.json();
+    })
+    .then(countries => { console.log(countries) })
+    .catch(error => { 
+        console.log(error);
+    });
+ 
